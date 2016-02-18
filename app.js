82fee40ae8f4184b57401562d894741c51a46b4a
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-02-18 16:17:24 -0700

    Added solution for basic express calculator

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var main = require('./main');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.get('/:operator/:num1/:num2', function (req, res) {
+    var operator = req.params.operator;
+    var num1 = req.params.num1;
+    var num2 = req.params.num2;
+    res.render('index', {result: main[operator](num1, num2)});
+});
+
+
+app.listen(3200, function () {
+    console.log('Starting a server on localhost:3200');
+});
---
+++ main.js
@@ -0,0 +1,24 @@
+function add (num1, num2) {
+    return +num1 + +num2;
+}
+
+function sub (num1, num2) {
+    return +num1 - +num2;
+}
+
+function mult (num1, num2) {
+    return +num1 * +num2;
+}
+
+function div (num1, num2) {
+    return +num1 / +num2;
+}
+
+
+
+module.exports = {
+    add: add,
+    sub: sub,
+    mult: mult,
+    div: div
+}
