52a6873e18766e331f0e4a6dd54a4f5495393ba7
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-02-22 09:47:14 -0700

    flipped the route around to have the operator in the middle

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 app.set('view engine', 'ejs');
 
 
-app.get('/:operator/:num1/:num2', function (req, res) {
+app.get('/:num1/:operator/:num2', function (req, res) {
     var operator = req.params.operator;
     var num1 = req.params.num1;
     var num2 = req.params.num2;
