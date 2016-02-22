15525ccf22e96b1e776ace28132b7e2094f8ead6
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-02-22 09:43:51 -0700

    Added power route

--- main.js
+++ main.js
@@ -14,11 +14,16 @@ function div (num1, num2) {
     return +num1 / +num2;
 }
 
+function pow (num1, num2) {
+    return Math.pow(+num1, +num2);
+}
+
 
 
 module.exports = {
     add: add,
     sub: sub,
     mult: mult,
-    div: div
+    div: div,
+    pow: pow
 }
